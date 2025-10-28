b70c69bb074557ab790d74177f0059a5a31bc132
Trần Tiến Hợp <trantienhop3302@gmail.com>
2025-10-28 10:18:45 +0700

    Add files via upload

---
+++ customer.h
@@ -0,0 +1,96 @@
+﻿#include<iostream>
+#include<string>
+#include <sstream>
+#include<fstream>
+#include <iomanip>
+#include"CTime.h"
+#include"CDate.h"
+#include"CName.h"
+#include"CAddress.h"
+
+using namespace std;
+
+class Person {
+protected:
+	CName name;
+	int mobile;
+	CDate dob;
+	string email;
+public:
+	Person();
+	Person(CName, int, CDate, string);
+	Person(const Person&);
+	virtual ~Person();
+
+	void set_name(CName);
+	void set_mobile(int);
+	void set_dob(CDate);
+	void set_email(string);
+
+	CName get_name();
+	int get_mobile();
+	CDate get_dob();
+	string get_email();
+
+	virtual void input() = 0;
+	virtual void display() = 0;
+	virtual void update() = 0;
+	virtual void edit() = 0;
+	virtual void remove() = 0;
+	virtual void save() = 0;
+};
+
+class Customer : public Person {
+private:
+	string customer_id;
+	int number_of_people;
+	double total_spending;
+
+public:
+	Customer();
+	Customer(string, string, int, string, string, int, double);
+	Customer(const Customer&);
+	~Customer();
+
+	void set_customer_id(string);
+	void set_number_of_people(int);
+	void set_total_spending(double);
+
+	string get_customer_id();
+	int get_number_of_people();
+	double get_total_spending();
+
+	void input();
+	void display();
+	void update();
+	void edit();
+	void remove();
+	void save();
+
+	void calculate_loyalty_points();
+	void view_booking();
+};
+
+
+struct Node {
+	Customer data;
+	Node* next;
+};
+
+class CustomerList {
+private:
+	Node* head;
+public:
+	CustomerList();
+	~CustomerList();
+
+	bool isEmpty();
+	void add_customer(const Customer&);
+	void display_all();
+	Node* search_by_id(string);
+	void update_customer(string);
+	void remove_customer(string);
+	void save_to_file(string);
+	void load_from_file(string);
+};
+
---
+++ customer_main.cpp
@@ -0,0 +1,80 @@
+﻿#include"customer.h"
+using namespace std;
+
+
+    int main() {
+        CustomerList customers;
+
+        // 1. Tự động đọc danh sách khách hàng từ file khi bắt đầu
+        customers.load_from_file("customer.txt");
+        cout << "Danh sach khach hang hien tai:\n";
+        customers.display_all();
+
+        int choice;
+        string id;
+
+        do {
+            cout << "\n===== QUAN LY KHACH HANG =====\n";
+            cout << "1. Them khach hang\n";
+            cout << "2. Hien thi tat ca khach hang\n";
+            cout << "3. Tim khach hang theo ID\n";
+            cout << "4. Cap nhat khach hang\n";
+            cout << "5. Xoa khach hang\n";
+            cout << "6. Luu danh sach vao file\n";
+            cout << "7. Doc danh sach tu file\n";
+            cout << "0. Thoat\n";
+            cout << "Chon chuc nang: ";
+            cin >> choice;
+            cin.ignore();
+
+            switch (choice) {
+            case 1: {
+                Customer c;
+                c.input();
+                customers.add_customer(c);
+                break;
+            }
+            case 2:
+                customers.display_all();
+                break;
+            case 3:
+                cout << "Nhap ID khach hang can tim: ";
+                getline(cin, id);
+                {
+                    Node* found = customers.search_by_id(id);
+                    if (found)
+                        found->data.display();
+                    else
+                        cout << "Khong tim thay khach hang!\n";
+                }
+                break;
+            case 4:
+                cout << "Nhap ID khach hang can cap nhat: ";
+                getline(cin, id);
+                customers.update_customer(id);
+                break;
+            case 5:
+                cout << "Nhap ID khach hang can xoa: ";
+                getline(cin, id);
+                customers.remove_customer(id);
+                break;
+            case 6:
+                customers.save_to_file("customer.txt");
+                cout << "Da luu danh sach vao customer.txt\n";
+                break;
+            case 7:
+                customers.load_from_file("customer.txt");
+                cout << "Da doc danh sach tu customer.txt\n";
+                break;
+            case 0:
+                cout << "Thoat chuong trinh.\n";
+                break;
+            default:
+                cout << "Lua chon khong hop le. Nhap lai!\n";
+            }
+        } while (choice != 0);
+
+        return 0;
+    }
+
+
