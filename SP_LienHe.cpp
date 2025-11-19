8d25b9f7cf3bef818e84388893f28e8ef66996ae
DoNhatThanh <donhatthanh20@gmail.com>
2025-11-19 18:35:18 +0700

    Add files via upload

---
+++ SP_LienHe.cpp
@@ -0,0 +1,21 @@
+#include "SP_LienHe.h"
+
+bool lienHe::kiemTraSDT(const string& sdt) {
+	if (sdt.length() != 10) return false;
+    if (sdt[0] != '0') return false;
+
+    for (char c : sdt) {
+        if (!isdigit(c)) return false;
+    }
+    return true;
+}
+
+bool lienHe::kiemTraEmail(const string& email) {
+	if (email.empty()) return false;
+    if (email.find(' ') != string::npos) return false;
+
+    size_t at = email.find('@');
+    size_t dot = email.find('.', at + 1);
+
+    return (at != string::npos && dot != string::npos && dot > at + 1);
+}
---
+++ SP_LienHe.h
@@ -0,0 +1,12 @@
+#pragma once
+#include<iostream>
+#include<cctype>
+#include<string>
+
+using namespace std;
+
+class lienHe {
+	public:
+		static bool kiemTraSDT(const string& sdt);
+		static bool kiemTraEmail(const string& email);
+};
